243b2fa2d474e418a2941b084e5cd35fa896bf68
jennilaluyan <laluyanjennifer11@gmail.com>
2025-03-12 13:14:56 +0800

    change readme file

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,6 @@ import './style.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1 className='font-bold text-5xl'>Hello World</h1>
+    <h1 className='font-bold text-6xl'>Hello World</h1>
   </StrictMode>,
 )
