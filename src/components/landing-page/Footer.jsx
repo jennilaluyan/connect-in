9695009d97a0b514d7437c16ec9dc95ac6ffe65d
Vicky Kaseger <vitokaseger02@gmail.com>
2025-03-13 14:36:01 +0800

    Added Responsiveness

--- src/components/landing-page/Footer.jsx
+++ src/components/landing-page/Footer.jsx
@@ -1,9 +1,11 @@
-
 const Footer = () => {
-  return (
-    <div className ="bg-blue-500 text-white text-[20px] w-screen absolute p-5 text-center left-0 margin mt-50">
-        <p> @ Connect IN - Project Framework Web 2025. </p> </div>
-  );
-};
-
-export default Footer;
+    return (
+      <section className="bg-blue-500 text-white text-[16px] sm:text-[18px] md:text-[20px] w-full absolute left-0 p-5 text-center mt-20 mx-0">
+        <div>
+          <p>@ Connect IN - Project Framework Web 2025.</p>
+        </div>
+      </section>
+    );
+  };
+  
+  export default Footer;
