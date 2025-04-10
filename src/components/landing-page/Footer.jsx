6e002657626b8ed877f9c60a5505ea4d2ecb189e
dortea ribka tadete <dortearibkatadete17.com>
2025-04-10 15:04:36 +0800

    Fix footer position

--- src/components/landing-page/Footer.jsx
+++ src/components/landing-page/Footer.jsx
@@ -1,11 +1,11 @@
 const Footer = () => {
-    return (
-      <section className="bg-blue-500 text-white text-[16px] sm:text-[18px] md:text-[20px] w-full absolute left-0 p-5 text-center mt-20 mx-0">
-        <div>
-          <p>@ Connect IN - Project Framework Web 2025.</p>
-        </div>
-      </section>
-    );
-  };
+  return (
+    <footer className="bg-blue-500 text-white text-[16px] sm:text-[18px] md:text-[20px] w-full p-5 text-center fixed bottom-0 left-0">
+      <div>
+        <p>@ Connect IN - Project Framework Web 2025.</p>
+      </div>
+    </footer>
+  );
+};
   
   export default Footer;
