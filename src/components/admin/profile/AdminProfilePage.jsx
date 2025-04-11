1939ef7812e8a38470740c0787b939e100a83a17
dortea ribka tadete <dortearibkatadete17.com>
2025-04-11 11:39:52 +0800

    profile admin

---
+++ src/components/admin/profile/AdminProfilePage.jsx
@@ -0,0 +1,13 @@
+import NavbarAdmin from "./NavbarAdmin"
+import ProfileCard from "./ProfileCard"
+
+function AdminProfilePage() {
+  return (
+    <>
+      <NavbarAdmin />
+      <ProfileCard />
+    </>
+  );
+}
+
+export default AdminProfilePage;
